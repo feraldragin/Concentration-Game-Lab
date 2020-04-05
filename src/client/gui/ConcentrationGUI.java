06c4a5c7c01215911abffad484cbc0c2ed5f1d20
iep1901 <iep1901@rit.edu>
2020-04-05 16:11:35 -0700

    Added to the init() and start() functions. Also created makeGridPane() and makeBorderPane() methods.

--- src/client/gui/ConcentrationGUI.java
+++ src/client/gui/ConcentrationGUI.java
@@ -7,7 +7,19 @@ import client.model.Observer;
 import common.ConcentrationException;
 
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
 import java.util.*;
 
@@ -19,8 +31,36 @@ import java.util.*;
  * @author YOUR NAME HERE
  */
 public class ConcentrationGUI extends Application implements Observer<ConcentrationModel, ConcentrationModel.CardUpdate> {
+    private HashMap pokemonMap;
+
+    private Image abra = new Image(getClass().getResourceAsStream("abra.png"));
+    private Image bulbasaur = new Image(getClass().getResourceAsStream("bulbasaur.png"));
+    private Image charizard = new Image(getClass().getResourceAsStream("charizard.png"));
+    private Image diglett = new Image(getClass().getResourceAsStream("diglett.png"));
+    private Image golbat = new Image(getClass().getResourceAsStream("golbat.png"));
+    private Image golem = new Image(getClass().getResourceAsStream("golem.png"));
+    private Image jigglypuff = new Image(getClass().getResourceAsStream("jigglypuff.png"));
+    private Image magikarp = new Image(getClass().getResourceAsStream("magikarp.png"));
+    private Image meowth = new Image(getClass().getResourceAsStream("meowth.png"));
+    private Image mewtwo = new Image(getClass().getResourceAsStream("mewtwo.png"));
+    private Image natu = new Image(getClass().getResourceAsStream("natu.png"));
+    private Image pidgey = new Image(getClass().getResourceAsStream("pidgey.png"));
+    private Image pikachu = new Image(getClass().getResourceAsStream("pikachu.png"));
+    private Image pokeball = new Image(getClass().getResourceAsStream("pokeball.png"));
+    private Image poliwag = new Image(getClass().getResourceAsStream("poliwag.png"));
+    private Image psyduck = new Image(getClass().getResourceAsStream("psyduck.png"));
+    private Image rattata = new Image(getClass().getResourceAsStream("rattata.png"));
+    private Image slowpoke = new Image(getClass().getResourceAsStream("slowpoke.png"));
+    private Image snorlax = new Image(getClass().getResourceAsStream("snorlak.png"));
+    private Image squirtle = new Image(getClass().getResourceAsStream("squirtle.png"));
+
+
+
+
+
     @Override
-    public void init() {
+    public void init() throws ConcentrationException {
+
         List<String> args = getParameters().getRaw();
 
         // get host and port from command line
@@ -29,14 +69,37 @@ public class ConcentrationGUI extends Application implements Observer<Concentrat
 
         // create the model, and add ourselves as an observer
         // TODO
-
+        ConcentrationModel model = new ConcentrationModel();
+        model.addObserver(this);
         // initiate the controller
         // TODO
+        ConcentrationController controller = new ConcentrationController(host, port, model);
     }
+
+    private GridPane makeGridPane(){
+        this.pokemonMap = new HashMap();
+
+        GridPane gridPane = new GridPane();
+        return gridPane;
+    }
+
+
+    private BorderPane makeBorderPane(ConcentrationModel.Status status, int moves, int matches){
+        BorderPane pane = new BorderPane();
+        Label label = new Label("Moves: " + moves + "       Matches: " + matches + "       Status: " + status);
+        pane.setCenter(makeGridPane());
+        pane.setBottom(label);
+        return pane;
+    }
+
 
     @Override
     public void start(Stage stage) throws Exception {
         // TODO
+        ConcentrationModel model = new ConcentrationModel();
+
+        Scene scene = new Scene(makeBorderPane(model.getStatus(), model.getNumMoves(), model.getNumMatches()));
+        stage.setScene(scene);
         stage.show();
     }
 
